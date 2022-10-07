1cc54721e72618ab53cc13d38a2414b8a87b5e8f
unknown <olivertrain1996@gmail.com>
2022-10-07 16:16:36 +0100

    First look at changing state
    Looking at changing state and the process oof declaring two variables as the built in react 'useState' it tells the app that the props.title info is being updated at some point! This then allows it to be turned into an array index 0=original 1=empty variable that can then be assigned to do a function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 // THESE ARE PROPS THE EVENT LISTNER ON THE HTML?JSX on click runs variable which is a function effectively
 function ExpenseItem(props) {
-    const changeTitle = () => {
-      console.log("test");
-    }
+    // Use state is a built in react that monitors for changes BUT have to pass in what is expected to be changed in this case the button to the title
+    // returns an array! previous value and the new version hence the 
+  const [title, setTitle] = useState(props.title);
+
+  // Click handler
+  const changeTitle = () => {
+    setTitle('updated');
+    console.log(title)
+  }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <p></p>
         <div className='expense-item__price test'>${props.amount}</div>
       </div>
